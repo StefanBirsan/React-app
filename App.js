c3a31e24e7eb0a3cc71ceb90e153250788d0a5fd
Stefan Birsan <83671730+StefanBirsan@users.noreply.github.com>
2023-11-18 00:19:54 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,145 +1,187 @@
-// App.js
-import React, { useState } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { CommonActions } from '@react-navigation/native';
+import MapView, {Callout, Marker} from "react-native-maps";
 
-const Stack = createStackNavigator();
+
+const Tab = createMaterialTopTabNavigator();
+
+const LoginButton = ({ navigation }) => {
+  const handleLoginPress = () => {
+    // Reset the navigation stack and navigate to the Login screen
+    navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [
+            { name: 'Login' },
+          ],
+        })
+    );
+  };
+
+  return (
+      <TouchableOpacity
+          style={styles.loginButton}
+          onPress={handleLoginPress}
+      >
+        <Text style={styles.lButtonText}>Login</Text>
+      </TouchableOpacity>
+  );
+};
 
 const HomeScreen = ({ navigation }) => {
-    const [showInitialPage, setShowInitialPage] = useState(true);
+  return (
+      <LinearGradient
+          colors={['#000000', '#A55233']}
+          style={styles.container}
+      >
+        <Text style={styles.textStyle}>
+          {"Dump Alert\n"}
+        </Text>
 
-    function CustomButton() {
-        return (
-            <TouchableOpacity
-                style={styles.customButtonContainer}
-                onPress={() => Alert.alert('Mi scarba de tine', 'Fuck off mate.')}
-            >
-                <Text style={styles.customButtonText}>Ce ai cu button-u meu bah?</Text>
-            </TouchableOpacity>
-        );
-    }
+        <CustomButton />
 
-    return (
-        <LinearGradient
-            // Background Linear Gradient
-            colors={['#884fcc', '#3fcffb', '#af1ddd']}
-            style={styles.container}
-        >
-            <Text style={styles.textStyle}>
-                {"Welcome to my shitty app\nMobile Applications Workshop"}
-            </Text>
+        {/* Add the LoginButton component */}
+        <LoginButton navigation={navigation} />
 
-            <TextInput
-                placeholder={"Type something here..."}
-                onChange={() => console.log("Some functionality here")}
-                style={styles.textInputStyle}
-            />
-
-            <Image
-                source={require("./assets/pozik.jpg")}
-                style={styles.imageStyle}
-            />
-
-            {/* Use navigation.navigate instead of setShowInitialPage */}
-            <CustomButton />
-            <TouchableOpacity
-                style={styles.customButtonContainer}
-                onPress={() => navigation.navigate('Second')}
-            >
-                <Text style={styles.customButtonText}>Go to Next Screen</Text>
-            </TouchableOpacity>
-
-            <StatusBar style="auto" />
-        </LinearGradient>
-    );
+        <StatusBar style="auto" />
+      </LinearGradient>
+  );
 };
 
 const SecondScreen = ({ navigation }) => {
-    return (
-        <LinearGradient
-            // Background Linear Gradient
-            colors={['#884fcc', '#3fcffb', '#af1ddd']}
-            style={styles.container}
+  return (
+      <LinearGradient
+          colors={['#000000', '#A55233']}
+          style={styles.container}
+      >
+        <TouchableOpacity
+            style={styles.customButtonContainer}
+            onPress={() => navigation.navigate('Home')}
         >
+          <Text style={styles.customButtonText}>Go to First Screen</Text>
+        </TouchableOpacity>
+      </LinearGradient>
+  );
+};
 
-            <TouchableOpacity
-                style={styles.customButtonContainer}
-                onPress={() => navigation.navigate('Home')}
+const LoginScreen = () => {
+  return (
+      <LinearGradient
+          colors={['#000000', '#A55233']}
+          style={styles.container}
+      >
+        {/* Your Login Screen components go here */}
+        <Text style={styles.textStyle}>Welcome to the Login Screen</Text>
+      </LinearGradient>
+  );
+};
+
+const MapScreen = () => {
+    const onRegionChange = (region) => {
+        console.log(region);
+    };
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                onRegionChange={onRegionChange}
+                initialRegion={{
+                  latitude: 45.7494,
+                    longitude: 21.2272,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
             >
-                <Text style={styles.customButtonText}>Go to First Screen</Text>
-            </TouchableOpacity>
-        </LinearGradient>
+            <Marker
+                coordinate={{
+                    latitude: 45.7494,
+                    longitude: 21.2272,
+                }}
+                pinColor="black"
+            >
+                <Callout>
+                    <Text>Ma-ta</Text>
+                </Callout>
+            </Marker>
+
+            </MapView>    
+        </View>
     );
 };
 
+const CustomButton = () => {
+  return (
+      <TouchableOpacity
+          style={styles.customButtonContainer}
+          onPress={() => Alert.alert('Mi scarba de tine', 'Fuck off mate.')}
+      >
+        <Text style={styles.customButtonText}>Ce ai cu button-u meu bah?</Text>
+      </TouchableOpacity>
+  );
+};
+
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="Home"
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: 'transparent', // Make the header background transparent
-                    },
-                    headerTintColor: '#000', // Change the text color of the header
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}
-            >
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Second" component={SecondScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+      <NavigationContainer>
+        <Tab.Navigator
+            tabBar={() => null} // Add this line
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Second" component={SecondScreen} />
+          <Tab.Screen name="Login" component={LoginScreen}/>
+          <Tab.Screen name="Map" component={MapScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-    },
-    textStyle: {
-        fontStyle: "italic",
-        fontSize: 22,
-        color: "#070708",
-        textAlign: "center",
-    },
-    textInputStyle: {
-        backgroundColor: "#b691ed",
-        padding: 10,
-        borderRadius: 10,
-        width: 200,
-        color: "#FFFFDD",
-        borderColor: "#000",
-        borderWidth: 3,
-        borderStyle: "solid",
-        marginTop: 20,
-    },
-    imageStyle: {
-        height: 200,
-        width: 200,
-        borderRadius: 100,
-        borderWidth: 3,
-        borderColor: "#000",
-    },
-    buttonsmek: {},
-    customButtonContainer: {
-        backgroundColor: 'blue', // Change the button color here
-        borderRadius: 20,
-        padding: 15,
-        width: 200,
-        alignItems: 'center',
-    },
-    customButtonText: {
-        color: 'white',
-        fontSize: 20, // Change the font size here
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  textStyle: {
+    fontStyle: 'italic',
+    fontSize: 22,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  customButtonContainer: {
+    backgroundColor: 'blue',
+    borderRadius: 20,
+    padding: 15,
+    width: 200,
+    alignItems: 'center',
+  },
+  customButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  loginButton: {
+    position: 'absolute',
+    top: 50, // Adjust top position as needed
+    right: 10, // Adjust right position as needed
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    padding: 10,
+    width: 100, // Adjust width as needed
+    alignItems: 'center',
+  },
+    lButtonText: {
+    color: 'black',
+        fontSize: 20,
         fontWeight: 'bold',
-        // Change the font family here
+    },
+    map: {
+        width: '100%',
+        height: '100%',
     },
 });
